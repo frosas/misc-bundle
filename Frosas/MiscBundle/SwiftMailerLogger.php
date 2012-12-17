a24559a514616a1c358e062f0f408e835664c74e
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-12-17 10:01:28 +0100

    Workaround to the ServiceCircularReferenceException

--- Frosas/MiscBundle/SwiftMailerLogger.php
+++ Frosas/MiscBundle/SwiftMailerLogger.php
@@ -8,9 +8,14 @@ class SwiftMailerLogger implements \Swift_Plugins_Logger
 {
     private $logger;
 
-    function __construct(LoggerInterface $logger)
+    function __construct(LoggerInterface $logger, \Swift_Transport $transport)
     {
         $this->logger = $logger;
+
+        // We register the plugin here and not using the swiftmailer.plugin tag
+        // to avoid the ServiceCircularReferenceException we get when Monolog is
+        // configured to mail the messages through SwiftMailer.
+        $transport->registerPlugin(new \Swift_Plugins_LoggerPlugin($this));
     }
 
     function add($entry)
