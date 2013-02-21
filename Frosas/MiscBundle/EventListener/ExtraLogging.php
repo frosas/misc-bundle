fa4c598be58e65970113fb3ce00dc0ad050d4e6f
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-02-21 12:41:34 +0100

    Request and exception details logging

---
+++ Frosas/MiscBundle/EventListener/ExtraLogging.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Frosas\MiscBundle\EventListener;
+
+use Frosas\Backtrace;
+use Frosas\Collection;
+use Frosas\String;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Bundle\FrameworkBundle\HttpKernel;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Logs requests and exceptions details
+ */
+class ExtraLogging
+{
+    private $logger;
+
+    function __construct($logger)
+    {
+        $this->logger = $logger;
+    }
+
+    function onKernelRequest(GetResponseEvent $event)
+    {
+        // Don't log sub-requests as these has the same information
+        if ($event->getRequestType() !== HttpKernel::MASTER_REQUEST) return;
+
+        $this->logger->debug("Request URI: {$event->getRequest()->getUri()}");
+        $this->logger->debug("Request headers:\n" . $this->dictionaryToString($event->getRequest()->headers));
+        if (count($get = $event->getRequest()->query)) {
+            $this->logger->debug("Request GET parameters:\n" . $this->dictionaryToString($get));
+        }
+        if (count($post = $event->getRequest()->request)) {
+            $this->logger->debug("Request POST parameters:\n" . $this->dictionaryToString($post));
+        }
+        if (count($attributes = $event->getRequest()->attributes)) {
+            $this->logger->debug("Request attributes:\n" . $this->dictionaryToString($attributes));
+        }
+        if (count($cookies = $event->getRequest()->cookies)) {
+            $this->logger->debug("Request cookies:\n" . $this->dictionaryToString($cookies));
+        }
+        $this->logger->debug("Request server:\n" . $this->dictionaryToString($event->getRequest()->server));
+    }
+
+    function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $backtrace = Backtrace::createFromException($event->getException());
+        $this->logger->debug("Exception trace:\n" . String::indent($backtrace));
+    }
+
+    private function dictionaryToString($dictionary)
+    {
+        return String::indent(Yaml::dump(Collection::toArray($dictionary), 1));
+    }
+}
